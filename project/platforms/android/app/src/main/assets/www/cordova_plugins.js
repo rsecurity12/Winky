82fb3c26170bdb273d434ad4a699713526618a54
Rodrigo <rodrigo.caldeirasilva@student.ap.be>
2022-10-31 15:54:27 +0100

    login page, frontend add

--- project/platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ project/platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -228,9 +228,42 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       "file": "plugins/cordova-plugin-googlemaps/www/js_CordovaGoogleMaps-for-android_ios.js",
       "pluginId": "cordova-plugin-googlemaps",
       "runs": true
+    },
+    {
+      "id": "org.apache.cordova.geolocation.Coordinates",
+      "file": "plugins/org.apache.cordova.geolocation/www/Coordinates.js",
+      "pluginId": "org.apache.cordova.geolocation",
+      "clobbers": [
+        "Coordinates"
+      ]
+    },
+    {
+      "id": "org.apache.cordova.geolocation.PositionError",
+      "file": "plugins/org.apache.cordova.geolocation/www/PositionError.js",
+      "pluginId": "org.apache.cordova.geolocation",
+      "clobbers": [
+        "PositionError"
+      ]
+    },
+    {
+      "id": "org.apache.cordova.geolocation.Position",
+      "file": "plugins/org.apache.cordova.geolocation/www/Position.js",
+      "pluginId": "org.apache.cordova.geolocation",
+      "clobbers": [
+        "Position"
+      ]
+    },
+    {
+      "id": "org.apache.cordova.geolocation.geolocation",
+      "file": "plugins/org.apache.cordova.geolocation/www/geolocation.js",
+      "pluginId": "org.apache.cordova.geolocation",
+      "clobbers": [
+        "navigator.geolocation"
+      ]
     }
   ];
   module.exports.metadata = {
-    "cordova-plugin-googlemaps": "2.7.1"
+    "cordova-plugin-googlemaps": "2.7.1",
+    "org.apache.cordova.geolocation": "0.3.6"
   };
 });
--- project/platforms/android/app/src/main/assets/www/js/index.js
+++ project/platforms/android/app/src/main/assets/www/js/index.js
@@ -26,4 +26,5 @@ function onDeviceReady() {
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     document.getElementById('deviceready').classList.add('ready');
+
 }
